0c0dd1f835b48936fe6d4861f18f3c034c96081a
Jonathan Goh <117988387+JonathanGoh95@users.noreply.github.com>
2025-06-02 10:46:47 +0800

    Updated NavBar links with NavLink

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,11 @@
-import { Link } from "react-router"
+import { NavLink } from "react-router"
 
 export default function NavBar() {
-
+    <nav>
+        <ul>
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/search">Search For Movies</NavLink></li>
+            <li><NavLink to="/favorites">Favorite Movies</NavLink></li>
+        </ul>
+    </nav>
 }
